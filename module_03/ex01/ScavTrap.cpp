a9408c74a15646bbbdead2d8fe552cee160ddf3f
Nonthakorn <nonthakorn.che@gmail.com>
2025-06-20 21:15:11 +0700

    modify destructor message

--- module_03/ex01/ScavTrap.cpp
+++ module_03/ex01/ScavTrap.cpp
@@ -50,7 +50,7 @@ ScavTrap:: ScavTrap(std::string name, int hitPoint, int energyPoint, int attackD
 }
 
 ScavTrap::~ScavTrap() {
-	std::cout << "Inherit Destrcutor is called" << std::endl;
+	std::cout << "Scav Trap Destrcutor is called" << std::endl;
 }
 
 ScavTrap& ScavTrap::operator=(const ScavTrap &rhs) {
