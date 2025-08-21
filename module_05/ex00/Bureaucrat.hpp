4c2067ba5cb74c0add69c9999da416d6b3afab1d
Nonthakorn <nonthakorn.che@gmail.com>
2025-08-21 13:56:05 +0700

    Clear

--- module_05/ex00/Bureaucrat.hpp
+++ module_05/ex00/Bureaucrat.hpp
@@ -4,33 +4,9 @@
 # include <iostream>
 
 class Bureaucrat {
-private:
-	const std::string _name;
-	int _grade;
 
-public:
-	Bureaucrat(void);
-	Bureaucrat(const std::string name, int grade);
-	Bureaucrat(const Bureaucrat &copy);
-	Bureaucrat& operator=(const Bureaucrat &rhs);
-	~Bureaucrat();
-
-	std::string getName() const;
-	int			getGrade() const;
-	void		setGrade(int grade);
-	void		increment_grade();
-	void		decrement_grade();
-
-	class GradeTooHighException: public std::exception {
-	public:
-		virtual const char* what() const throw();
-	};
-	class GradeTooLowException: public std::exception {
-	public:
-		virtual const char* what() const throw();
-	};
 };
 
-std::ostream& operator<<(std::ostream &out, Bureaucrat const &bureaucrat);
+
 
 #endif
--- module_05/ex01/Form.hpp
+++ module_05/ex01/Form.hpp
@@ -8,37 +8,8 @@
 class Bureaucrat;
 
 class Form {
-private:
-	const std::string	_name;
-	bool				_isSigned;
-	const int			_signGrade;
-	const int			_executeGrade;
-public:
-	Form(void);
-	Form(const std::string name, const int gradeSign, const int gradeExecute);
-	Form(const Form &cpy);
-	Form& operator=(const Form &rhs);
-	~Form();
 
-	std::string	getName() const;
-	bool		getSignStatus() const;
-	int			getGradeSign() const;
-	int			getGradeExecute() const;
-
-	void		beSigned(Bureaucrat &bureaucrat);
-	void		signedForm();
-
-	class GradeTooHighException: public std::exception {
-	public:
-	 virtual const char* what() const throw();
-	};
-
-	class GradeTooLowException: public std::exception {
-	public:
-	 virtual const char* what() const throw();
-	};
 };
 
-std::ostream& operator<<(std::ostream &out, Form const &form);
 
 # endif
